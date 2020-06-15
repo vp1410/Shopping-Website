c1f8154b9ba9f9a55c19309b08534e516f7eec8e
Vivek Panchal <vivekpanchal@Viveks-MacBook-Pro.local>
2020-06-15 16:17:12 -0400

    RemoveItem at checkout

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './homepage.styles.scss';
 import Directory from '../../components/directory/directory.component';
-import MenuItem from '../../components/menu-item/menu-item.component';
+// import MenuItem from '../../components/menu-item/menu-item.component';
 const HomePage = ()=>(
     <div className='homepage'>
         <Directory />
