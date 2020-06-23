6890279eacbfd78bc34835c1b0d4900d6cad7b71
Vivek Panchal <vivekpanchal@Viveks-MacBook-Pro.local>
2020-06-23 14:45:01 -0400

    Directory changes

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
+import { Route } from 'react-router-dom';
 
-const ShopPage =({ collections }) => (
-    <div className='shop-page'> 
-        <CollectionsOverview />
-    </div>
+const ShopPage =({ match }) => (
+     <div className='shop-page'> 
+            <Route exact path = {`${match.path}`} component={CollectionsOverview} />
+            <Route path = {`${match.path}/:collectionId`} component={CollectionPage} />
+        </div>
     );
+    
 
 export default ShopPage;
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -6,3 +6,16 @@ export const selectCollections = createSelector(
     [selectShop],
     shop => shop.collections
 );
+
+export const selectCollectionsForPreview = createSelector(
+    [selectCollections],
+    collections => Object.keys(collections).map(key => collections[key])
+);
+
+// Find collection.id matching to the url parameter of our collection.id map
+export const selectCollection = collectionUrlParam => 
+    createSelector(
+        [selectCollections],
+        collections => 
+            collections[collectionUrlParam]
+    );
